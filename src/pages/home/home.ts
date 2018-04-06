c70b1ee15e95778218d57febe179b03e44079394
rahmakevo <siundu344@gmail.com>
2018-04-06 19:25:42 +0300

    working on fetching api data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  weather: any;
+  location : {
+    city : string;
+  }
 
+  constructor(public navCtrl: NavController, private weatherProvider: WeatherProvider) {
+
+  }
+
+  ionViewWillEnter() {
+    this.location = {
+      city: 'Miami'
+    }
+    this.weatherProvider.getWeather(this.location.city)
+    .subscribe(weather => {
+      console.log(weather);
+      this.weather = weather.Object;  
+    });
   }
 
 }
